8ba794a03872789584934a1f2560a6798c443eb8
Camille Jian <camille66071@gmail.com>
2026-04-30 17:41:06 +1000

    Initial setup

---
+++ TimerApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimerApp
+//
+//  Created by Camille Jian on 30/4/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimerApp/TimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAppApp.swift
+//  TimerApp
+//
+//  Created by Camille Jian on 30/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct TimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
