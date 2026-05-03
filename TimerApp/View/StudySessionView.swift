fb2b18f2fe1c8793c63cc88aff02b65de88d9001
Camille Jian <camille66071@gmail.com>
2026-05-03 20:16:54 +1000

    Timer screen

--- TimerApp/View/StudySessionView.swift
+++ TimerApp/View/StudySessionView.swift
@@ -8,8 +8,145 @@
 import SwiftUI
 
 struct StudySessionView: View {
+    @StateObject private var viewModel = TimerViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            LinearGradient(
+                colors: viewModel.currentMode == .study ? [Color.blue.opacity(0.1), Color.purple.opacity(0.1)] : [Color.green.opacity(0.1), Color.yellow.opacity(0.1)],
+                
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+            .ignoresSafeArea()
+            
+            VStack(spacing: 30) {
+                HStack {
+                    Image(systemName: viewModel.currentMode == .study ? "brain.head.profile" : "cup.and.saucer.fill")
+                        .font(.title2)
+                    Text(viewModel.currentMode == .study ? "Focusing" : "Resting")
+                        .font(.title2.bold())
+                }
+                .padding(.horizontal, 20)
+                .padding(.vertical, 10)
+                .background(
+                    Capsule()
+                        .fill(viewModel.currentMode == .study ? Color.blue.opacity(0.2) : Color.green.opacity(0.2))
+                        
+                )
+                .padding(.top, 40)
+                
+                Spacer()
+                
+                
+                ZStack {
+                    Circle()
+                        .stroke(lineWidth: 20)
+                        .opacity(0.1)
+                        .foregroundColor(viewModel.currentMode == .study ? .blue : .green)
+                    
+                    
+                    Circle()
+                        .trim(from: 0, to: viewModel.progress)
+                        .stroke(style: StrokeStyle(lineWidth: 20, lineCap: .round))
+                        .foregroundColor(viewModel.currentMode == .study ? .blue : .orange)
+                        .rotationEffect(.degrees(-90))
+                        .animation(.linear, value: viewModel.progress)
+                    
+                    
+                    VStack(spacing: 10) {
+                        Text(viewModel.eggIcon())
+                            .font(.system(size: 80))
+                        
+                        Text(viewModel.timeString)
+                            .font(.system(size: 48, weight: .bold, design: .monospaced))
+                        
+                        Text(viewModel.currentMode == .study ? "Study time" : "Rest time")
+                            .font(.caption)
+                            .foregroundColor(.secondary)
+                    }
+                }
+                .frame(width: 280, height: 280)
+                .padding()
+                
+                Spacer()
+                
+                
+                
+                HStack(spacing: 40) {
+                    if viewModel.currentMode == .rest && viewModel.isRunning {
+                        Button(action: {viewModel.skipRest()}) {
+                            VStack {
+                                Image(systemName: "forward.fill")
+                                    .font(.title2)
+                                Text("Skip")
+                                    .font(.caption)
+                            }
+                            .foregroundColor(.orange)
+                        }
+                    }
+                    
+                    Button(action: {
+                        if viewModel.isRunning {
+                            viewModel.pauseTimer()
+                        } else {
+                            viewModel.startTimer()
+                        }
+                    }) {
+                        ZStack {
+                            Circle()
+                                .fill(viewModel.currentMode == .study ? Color.blue : Color.green)
+                                .frame(width: 80, height: 80)
+                                .shadow(radius: 10)
+                            
+                            Image(systemName: viewModel.isRunning ? "pause.fill" : "play.fill")
+                                .font(.title)
+                                .foregroundColor(.white)
+                            
+                        }
+                    }
+                    
+                    if viewModel.currentMode == .study && !viewModel.isRunning {
+                        Button(action: {viewModel.resetTimer()}) {
+                            VStack {
+                                Image(systemName: "arrow.counterclockwise")
+                                    .font(.title2)
+                                Text("Reset")
+                                    .font(.caption)
+                            }
+                            .foregroundColor(.red)
+                        }
+                    }
+                }
+                .padding(.bottom, 40)
+                
+                // Just for testing the rest button functions
+                #if DEBUG
+                Button(action: {viewModel.forceCompleteSession()}) {
+                    Text("Testing functions")
+                        .font(.caption)
+                        .foregroundColor(.orange)
+                }
+                #endif
+                
+                HStack {
+                    Image(systemName: "Info.circle")
+                    Text(viewModel.currentMode == .study ? "Next: Rest \(viewModel.restDuration) mins" : "Next: New Study Session \(viewModel.studyDuration) mins")
+                        .font(.caption)
+                }
+                .foregroundColor(.secondary)
+                .padding(.bottom, 20)
+            }
+        }
+        .alert("Need a longer break!!", isPresented: $viewModel.showingLongBreakAlert) {
+            Button("15 mins break", role: .cancel) {
+                viewModel.startLongBreak()
+            }
+            Button("Continue", role: .destructive) { }
+        } message: {
+            Text("Congrat! You finished \(viewModel.totalCycles) study cycles!! Recommend resting for 10 - 15 mins")
+        }
+        
     }
 }
 
--- TimerApp/ViewModel/TimerViewModel.swift
+++ TimerApp/ViewModel/TimerViewModel.swift
@@ -5,4 +5,166 @@
 //  Created by Florence Fendy on 30/4/2026.
 //
 
-import Foundation
+import SwiftUI
+import Combine
+
+enum TimerMode {
+    case study, rest
+}
+
+class TimerViewModel: ObservableObject {
+    
+    @Published var timeRemaining: Int = 25 * 60
+    
+    @Published var currentMode: TimerMode = .study
+    
+    @Published var isRunning = false
+    
+    @Published var totalPoints = 0
+    
+    @Published var eggStage = 1
+    
+    @Published var totalStudySessions = 0
+    
+    @Published var showingLongBreakAlert = false
+    
+    
+    @Published var studyDuration: Int = 25
+    
+    @Published var restDuration: Int = 5
+    
+    @Published var totalCycles: Int = 4
+    
+    
+    private var timer: AnyCancellable?
+    
+    private var successfulSessions = 0
+    
+    private var failedSessions = 0
+    
+    
+    var timeString: String {
+        let minutes = timeRemaining / 60
+        let seconds = timeRemaining % 60
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
+    
+    var progress: Double {
+        let total = currentMode == .study ? studyDuration * 60 : restDuration * 60
+        return 1 - (Double(timeRemaining) / Double(total))
+    }
+    
+    var sucessRate: Double {
+        let total = successfulSessions + failedSessions
+        return total > 0 ? (Double(successfulSessions) / Double(total)) * 100 : 100
+    }
+    
+    func eggIcon() -> String {
+        switch eggStage {
+        case 1: return "🥚"
+        case 2: return "🥚🔍"
+        case 3: return "🐱🥚"
+        case 4: return "🐱"
+        default: return "🥚"
+        }
+    }
+    
+    func startTimer() {
+        isRunning = true
+        timer = Timer.publish(every: 1, on: .main, in: .common)
+            .autoconnect()
+            .sink { [weak self] _ in
+                self?.tick()
+            }
+    }
+    
+    func pauseTimer() {
+        isRunning = false
+        timer?.cancel()
+    }
+    
+    
+    func resetTimer() {
+        pauseTimer()
+        timeRemaining = currentMode == .study ? studyDuration * 60 : restDuration * 60
+    }
+    
+    func skipRest() {
+        guard currentMode == .rest else { return }
+        pauseTimer()
+        completeRestSession()
+    }
+    
+    func forceCompleteSession() {
+        pauseTimer()
+        
+        if currentMode == .study {
+                completeStudySession()
+            } else {
+                completeRestSession()
+            }
+    }
+    
+    private func tick() {
+        guard timeRemaining > 0 else {
+            completeSession()
+            return
+        }
+        timeRemaining -= 1
+    }
+    
+    private func completeSession() {
+        pauseTimer()
+        
+        if currentMode == .study {
+            completeSession()
+        } else {
+            completeRestSession()
+        }
+    }
+    
+    private func completeStudySession() {
+        totalStudySessions += 1
+        successfulSessions += 1
+        totalPoints += 10
+        
+        if totalStudySessions % 2 == 0 && eggStage < 4 {
+            eggStage += 1
+            totalPoints += 5
+        }
+        
+        if totalStudySessions >= totalCycles {
+            showingLongBreakAlert = true
+        } else {
+            currentMode = .rest
+            timeRemaining = restDuration * 60
+            startTimer()
+        }
+    }
+    
+    private func completeRestSession() {
+        currentMode = .study
+        timeRemaining = studyDuration * 60
+        startTimer()
+    }
+    
+    func startLongBreak() {
+        showingLongBreakAlert = false
+        currentMode = .rest
+        timeRemaining = 15 * 60
+        startTimer()
+    }
+    
+    
+    func startNewSession() {
+        currentMode = .study
+        timeRemaining = studyDuration * 60
+        isRunning = false
+        totalStudySessions = 0
+        successfulSessions = 0
+        eggStage = 1
+        totalPoints = 0
+        failedSessions = 0
+        timer?.cancel()
+    }
+}
